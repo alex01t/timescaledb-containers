450cacced462f0c38f84339229617e1ed382eaf6
Alexander Tarasenko <tarasenkoas@gmail.com>
2026-08-03 16:30:10 +0300

    +

--- x/x.sh
+++ x/x.sh
@@ -1,6 +1,8 @@
 
 
-d build . 
 
-d push tarasenkoas/cloudnative-pg-postgresql-timescaledb:18.4
+d build . -t tarasenkoas/cloudnative-pg-postgresql-timescaledb:18.4-ts2.29.0-debian13
+
+d push tarasenkoas/cloudnative-pg-postgresql-timescaledb:18.4-ts2.29.0-debian13 
+
 
